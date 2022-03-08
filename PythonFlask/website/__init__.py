107b91c2aa73d912188d8b8aa0e86d1eabf084d8
Jose Servin <Servinjose96@gmail.com>
2022-03-08 16:43:03 -0600

    defined database in models.py file

--- PythonFlask/website/__init__.py
+++ PythonFlask/website/__init__.py
@@ -4,12 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 DB_NAME = 'database.db'
 
+
 def create_app():
     app = Flask(__name__)  # needed as a default setup for Flask
     app.config['SECRET_KEY'] = 'BakerCamilaBella'
     app.config['SQLALCHEMY_DATABASE_URL'] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
-
 
     # IMPORTING BLUEPRINTS TO __INIT__ FILE
     from .views import views
@@ -18,6 +18,5 @@ def create_app():
     # REGISTER BLUEPRINTS WITH OUT FLASK APPLICATION
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-
 
     return app
