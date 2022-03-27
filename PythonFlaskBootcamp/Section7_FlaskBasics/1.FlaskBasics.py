e9ba0bcef197d555d56d39332d4fc66c70623bca
Jose Servin <Servinjose96@gmail.com>
2022-03-27 14:16:06 -0500

    flaskbasics.py completed build to create simple first and last name gathering, starting flask_username_check file for Flask Template Exercise

--- PythonFlaskBootcamp/Section7_FlaskBasics/1.FlaskBasics.py
+++ PythonFlaskBootcamp/Section7_FlaskBasics/1.FlaskBasics.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,21 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/user/<name>')
-def user_page(name):
-    return render_template('user.html', name=name)
+@app.route('/signup')
+def sign_up():
+    return render_template('signup.html')
+
+
+@app.route('/thankyou')
+def thank_you():
+    first = request.args.get('first')
+    last = request.args.get('last')
+    return render_template('thankyou.html', first=first, last=last)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
