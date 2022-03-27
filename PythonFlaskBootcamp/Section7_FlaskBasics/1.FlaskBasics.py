23927d874c53d97c26862e62b9ad16992a4d3cf5
Jose Servin <Servinjose96@gmail.com>
2022-03-26 19:18:40 -0500

    setting up template inheritance using base.html

--- PythonFlaskBootcamp/Section7_FlaskBasics/1.FlaskBasics.py
+++ PythonFlaskBootcamp/Section7_FlaskBasics/1.FlaskBasics.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')  # this renders at http://127.0.0.1:5000/
-def index():
-    name = 'Servin'
-    name_list = list(name)
-    return render_template('index.html', name=name, name_list=name_list)
+def home():
+    return render_template('home.html')
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return render_template('user.html', name=name)
 
 
 if __name__ == "__main__":
