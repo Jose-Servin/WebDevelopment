30a690416d9f0721bc0f770b1756005ffc23e9a2
Jose Servin <Servinjose96@gmail.com>
2022-07-08 16:31:00 -0500

    proper template inheritance setup using base, home and user pages

--- flaskenv/basics.py
+++ flaskenv/basics.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('test.html')
+    return render_template('home.html')
+
+
+@app.route('/user/<user_name>')
+def template_inheritance_demo(user_name):
+    user_name = user_name
+    return render_template('user.html', user_name=user_name)
 
 
 if __name__ == '__main__':
