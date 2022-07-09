9e1a66d2495809a44b59252b20a6e687b6d1918a
Jose Servin <Servinjose96@gmail.com>
2022-07-09 14:03:08 -0500

    new notes and template forms and adding a simple username checker

--- flaskenv/basics.py
+++ flaskenv/basics.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import re
 
 app = Flask(__name__)
 
@@ -8,16 +9,28 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/signup-page')
-def signup():
-    return render_template('signup.html')
+@app.route('/report')
+def report():
+    username = request.args.get('username')
+
+    has_uppercase = False
+    has_lowercase = False
+    has_number = False
+
+    has_uppercase = any(i.isupper() for i in username)
+    has_lowercase = any(i.islower() for i in username)
+    has_number = username[-1].isdigit()
+
+    valid_username = has_number and has_uppercase and has_lowercase
+
+    return render_template('signup.html', valid_username=valid_username, has_number=has_number,
+                           has_lowercase=has_lowercase,
+                           has_uppercase=has_uppercase)
 
 
-@app.route('/thankyou')
-def thankyou():
-    first_name = request.args.get('fname')
-    last_name = request.args.get('lname')
-    return render_template('thankyou.html', first_name=first_name, last_name=last_name)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404_error.html'), 404
 
 
 if __name__ == '__main__':
