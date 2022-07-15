f901cc04c3c74554e746684b8d65d1f9b4f1ed68
Jose Servin <Servinjose96@gmail.com>
2022-07-15 16:14:20 -0500

    set up Flask Migration for Department database table

--- flaskenv/basics.py
+++ flaskenv/basics.py
@@ -1,22 +1,25 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 # __file__ --> basics.py
 # the os.path.dirname is flaskenv/basics.py
 # the abspath is the "full path" which when printed is /Users/joseservin/WebDevelopment/flaskenv
 
-# 1. Create Flask App
+# CREATING FLASK APP
 app = Flask(__name__)
 
-# 2. Configure Flask App for SQLAlchemy
+# SQLALCHEMY CONFIGURATIONS
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# 3. Pass our application into SQLAlchemy class call
-
+# DEFINING DATABASE
 db = SQLAlchemy(app)
+
+# MIGRATING APP AND DB
+Migrate(app, db)
 
 
 # HOW TO CREATE A MODEL
@@ -25,11 +28,13 @@ class Departments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     security_level = db.Column(db.Integer)
+    incentive = db.Column(db.BOOLEAN)
 
-    def __init__(self, name, security_level):
+    def __init__(self, name, security_level, incentive):
         self.name = name
         self.security_level = security_level
+        self.incentive = incentive
 
     def __repr__(self):
-        rep = f"The {self.name} has a security level of {self.security_level}. "
+        rep = f"The {self.name} has a security level of {self.security_level}. Incentive? {self.incentive} "
         return rep
