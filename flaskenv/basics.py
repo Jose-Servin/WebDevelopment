cded64cfd0b99d612e5f1db1d5900c394e612ac3
Jose Servin <Servinjose96@gmail.com>
2022-07-14 21:41:11 -0500

    set up db using SQLAlchemy in basics.py file with notes in MD file containing code explanation

--- flaskenv/basics.py
+++ flaskenv/basics.py
@@ -1,33 +1,35 @@
-from flask import Flask, render_template, flash, session, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# Next we set up our application and security key:
+basedir = os.path.abspath(os.path.dirname(__file__))
+# __file__ --> basics.py
+# the os.path.dirname is flaskenv/basics.py
+# the abspath is the "full path" which when printed is /Users/joseservin/WebDevelopment/flaskenv
+
+# 1. Create Flask App
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'mysecretkey123'
+# 2. Configure Flask App for SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join((basedir, 'data.sqlite'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# 3. Pass our application into SQLAlchemy class call
+
+db = SQLAlchemy(app)
 
 
-# We move on to creating a simple form.
-class SimpleForm(FlaskForm):
-    department = StringField('What department are you in? ')
-    submit = SubmitField('Submit')
+# HOW TO CREATE A MODEL
+class Departments(db.Model):
+    # create columns for table
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    security_level = db.Column(db.Integer)
 
+    def __init__(self, name, security_level):
+        self.name = name
+        self.age = security_level
 
-# here we define our index view function
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = SimpleForm()
-
-    if form.validate_on_submit():
-        session['department'] = form.department.data
-        flash(f"You are in the {session['department']} department. ")
-
-        return redirect(url_for('index'))
-
-    return render_template('home.html', form=form)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    def __repr__(self):
+        rep = f"The {self.name} has a security level of {self.security_level}. "
+        return rep
