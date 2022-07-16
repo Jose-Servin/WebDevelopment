04ff45639607760e2017a0ad7251b1b9d93c5c70
Jose Servin <Servinjose96@gmail.com>
2022-07-16 15:37:45 -0500

    completed Models set up for Departments, Employees and Manager as well as Migration process

--- flaskenv/basics.py
+++ flaskenv/basics.py
@@ -29,6 +29,11 @@ class Departments(db.Model):
     name = db.Column(db.Text)
     security_level = db.Column(db.Integer)
     incentive = db.Column(db.BOOLEAN)
+    # ONE-TO-MANY RELATIONSHIP (DEPARTMENT --> EMPLOYEES)
+    employees = db.relationship('Employees', backref='department', lazy='dynamic')
+
+    # ONE-TO-ONE RELATIONSHIP (DEPARTMENT --> MANAGER)
+    manager = db.relationship('Manager', backref='department', uselist=False)
 
     def __init__(self, name, security_level, incentive):
         self.name = name
@@ -36,5 +41,47 @@ class Departments(db.Model):
         self.incentive = incentive
 
     def __repr__(self):
-        rep = f"The {self.name} has a security level of {self.security_level}. Incentive? {self.incentive} "
-        return rep
+        if self.manager:
+            return f"Department name: {self.name} | Manager {self.manager.first_name} {self.manager.last_name}"
+            # self.manager.name is an attribute from the Manager class
+        else:
+            return f"{self.name} department has no Manager assigned. "
+
+    def report_employees(self):
+        print(f"{self.name} department has these employees: ")
+        for i in self.employees:
+            print(f"{i.first_name} {i.last_name}")  # this is an attribute from the Employees class
+
+
+class Employees(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+
+    # CONNECTING EMPLOYEES WITH DEPARTMENTS
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
+
+    def __init__(self, first_name, last_name, department_id):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.department_id = department_id
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name} Department ID {self.department_id}"
+
+
+class Manager(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+
+    # CONNECTING DEPARTMENT WITH ONE MANAGER
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
+
+    def __init__(self, first_name, last_name, department_id):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.department_id = department_id
+
+    def __repr__(self):
+        return f"Manager: {self.first_name} {self.last_name} work in Department ID {self.department_id}"
